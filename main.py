c36e1497c377918ef3f161df9e7d03cb4de81544
Samira <tisamirsasilva@gmail.com>
2021-12-24 17:59:37 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f' No mês de {mes} alguém bateu a meta de vendas e está apto para receber o bônus. Vendedor: {vendedor}, Vendas: {vendas}')
+
